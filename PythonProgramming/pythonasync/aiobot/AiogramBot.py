4b1b974d2946ba07f77585140383fa963f8181ad
Valerii Tinkevich <barm2001@gmail.com>
2023-11-28 13:41:55 +0700

    aiogram bot update

--- PythonProgramming/pythonasync/aiobot/AiogramBot.py
+++ PythonProgramming/pythonasync/aiobot/AiogramBot.py
@@ -8,3 +8,24 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer("Привет я глупый бот который пока ничего не умеет")
+    
+
+@dp.message(Command(commands=["help"]))
+async def process_help_command(message: Message):
+    await message.answer("пака ничиво")
+    
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+    
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
